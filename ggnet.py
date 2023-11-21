80795c788195cf6f66d81dd6a8ec497644cf2575
Eduardo Calderón <46823752+EduardoCalderon9@users.noreply.github.com>
2023-11-21 07:58:16 -0600

    Add files via upload

--- ggnet.py
+++ ggnet.py
@@ -1,10 +1,18 @@
 import streamlit as st
+import requests
+from openai import OpenAI
+
+
+client = OpenAI(api_key='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') 
+
 
 st.title('GGNET')
 st.header('Manejo de APIs de Facebook, LinkedIn y ChatGPT')
 
-selection = st.selectbox('Seleccionar', ['Envio de Correos', 'Creacion de Anuncios'])
 
+
+selection = st.selectbox('Seleccionar', ['Envio de Correos', 'Creacion de Anuncios'])
+print('render')
 if selection == 'Envio de Correos':
     destinatario = st.text_input('Ingrese un destinatario')
     placeholder = st.empty()
@@ -19,19 +27,30 @@ if selection == 'Creacion de Anuncios':
     ad_topic = st.selectbox('Seleccionar tema del anuncio', ['Cloud Hosting', 'Desarrollo de Software', 'Capacitaciones', 'Plan especifico'])
     title_placeholder= st.empty()
     generate_title = st.button('Generar Titulo', key=3)
-    description_placeholder= st.empty()
-    generate_description= st.button('Generar Descripcion', key=4)
     content_placeholder = st.empty()
     generate_content = st.button('Generar Contenido', key=5)
     ad_title = title_placeholder.text_input('Titulo del Anuncio')
-    ad_description = description_placeholder.text_area('Descripcion del Anuncio')
     ad_content = content_placeholder.text_area('Contenido del Anuncio')
     if generate_title:
-        ad_title = title_placeholder.text_input('Titulo del Anuncio', value='GGNET, tu aliado en tecnologia.', key=6)
-    if generate_description:
-        ad_description = description_placeholder.text_area('Descripcion del Anuncio', value='Descubre el futuro de tu negocio con GGNet. Ofrecemos servicios de nube de última generación, soporte técnico 24/7 y desarrollo de software personalizado. Desde la nube hasta soluciones a medida, estamos listos para impulsar tu éxito. ¡Contáctanos hoy!', key=7)
+        completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Generas textos para correos y anuncios para una empresa llamada GGNET. GGNET es una empresa que ofrece distintos servicios en la nube, estos incluyen Hosting confiable y seguro; Imágenes de Ubuntu, Debian, CentOs y demás sistemas operativos; Migraciones, Snapshots, Almacenamiento, Balanceadores de carga , Dominios y SSL. GGNET tambien ofrece servicios en la nube ofrecen el servicio de desarrollo de software que incluye desarrollo web, movil y de escritorio, ademas de capacitaciones y seguimiento de proyectos de software. El slogan de la empresa es 'Tu aliado estrategico en tecnologia' "},
+            {"role": "user", "content": "Escribe el titulo para un post en linkedIn sobre los servicios de desarrollo de software. El titulo debe ser conciso debe incluir emojis."}
+
+        ]
+        )
+        ad_title = title_placeholder.text_input('Titulo del Anuncio', value=completion.choices[0].message.content, key=6)
     if generate_content:
-        ad_content = content_placeholder.text_area('Contenido del Anuncio', value='🚀💻 ¡GGNet: tu aliado tecnológico para el éxito empresarial! 💡🌐\n\nEn GGNet entendemos que la tecnología es fundamental para el crecimiento y el éxito de tu empresa. Por eso, ofrecemos una amplia gama de servicios de consultoría y soluciones de infraestructura de TI 🔧🌐, para que puedas aprovechar al máximo todo el potencial de la era digital.', key=8)
+        completion_content= client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Generas textos para correos y anuncios para una empresa llamada GGNET. GGNET es una empresa que ofrece distintos servicios en la nube, estos incluyen Hosting confiable y seguro; Imágenes de Ubuntu, Debian, CentOs y demás sistemas operativos; Migraciones, Snapshots, Almacenamiento, Balanceadores de carga , Dominios y SSL. GGNET tambien ofrece servicios en la nube ofrecen el servicio de desarrollo de software que incluye desarrollo web, movil y de escritorio, ademas de capacitaciones y seguimiento de proyectos de software. El slogan de la empresa es 'Tu aliado estrategico en tecnologia' "},
+            {"role": "user", "content": "Escribe el texto para un post en linkedIn sobre los servicios de desarrollo de software. El texto no debe ser muy largo y debe incluir emojis."}
+        ]
+        )
+   
+        ad_content = content_placeholder.text_area('Contenido del Anuncio', value=completion_content.choices[0].message.content.replace('\n', '  \n'), key=8)
 
 
     if app == 'Facebook':
@@ -52,4 +71,18 @@ if selection == 'Creacion de Anuncios':
         </style>""", unsafe_allow_html=True)
     b = st.button("Publicar Anuncio")
     if b:
+        linkedInRequest = requests.post('https://api.linkedin.com/rest/posts?oauth2_access_token=AQVRSeKjU8njn_XWZ93Jad8mBf31qpiZyWc996DR17eGk-cfKV084Qs3OpdulIXGKPweU4M6YeeoIrg7SzXhNqcETaoqkyF763mUxXwd9zosH17FpVA_zNbGBzliCpCIH_VEHA9botL4rm9hmEAx02KznliNlNcHHFIzMU5Uap3lmpyd9yzy9VaB2myVlVAA_Nx65UOamJPbLnd_674uTBl86W2b1mFildud53r2RDwR8kvCGlDb-OWPAoN5KQGkSseCzRzXp4pLHrfJSES3K7WubHBedS2rsyZU-AkLPMN3zXu-28ChwTjYswRZSALZLV4d9yGfk6VRc_-cfHTh77qVNzakdg',
+                    {
+                    "author": "urn:li:organization:100145909",
+                    "commentary": f'{ad_title}\n\n{ad_content}',
+                    "visibility": "PUBLIC",
+                    "distribution": {
+                        "feedDistribution": "MAIN_FEED",
+                        "targetEntities": [],
+                        "thirdPartyDistributionChannels": []
+                    },
+                    "lifecycleState": "PUBLISHED",
+                    "isReshareDisabledByAuthor": False
+                    })
+        print(linkedInRequest)
         st.success('El anuncio fue enviado y esta en espera de aprobación')
