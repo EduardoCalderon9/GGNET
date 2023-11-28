35ea562fc6c7f1d388bdb5395ba6add24eb7dda0
Eduardo <eduardo.calderon@galileo.edu>
2023-11-27 21:41:35 -0600

    video

---
+++ pages/Obtener_Llave_de_OpenAI.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('¿Como obtener llave del API de OpenAI?')
+
+st.write('Para obtener una llave del API de OpenAI y utilizarla para generar textos debes iniciar navegando a https://platform.openai.com/api-keys')
+
+st.write('Selecciona la opcion de crear nueva llave secreta, dale un nombre a tu nueva llave si deseas y selecciona crear nueva llave')
